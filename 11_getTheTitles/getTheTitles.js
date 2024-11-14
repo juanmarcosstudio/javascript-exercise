e7ff286f6a5ee4b4a9e6bffadc447b2e9408e1e2
Jhon Mark <jhonmarktamayo134@gmail.com>
2024-11-14 19:47:18 +0700

    11_getTitles: Add getTitles to retrieve book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrray) {
+    return Array.map((book) => book.title);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// Example usage
+//const books = [
+//    { title: 'Book', author: 'Name' },
+//    { title: 'Book2', author: 'Name2' }
+//];
+
+//console.log(getTheTitles(book));
